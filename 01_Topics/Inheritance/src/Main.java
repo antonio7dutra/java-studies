6b4ce3ee3ad46b5980e6b2db3223e4d5ffdc016d
Antonio <antonio7dutra@gmail.com>
2026-01-11 20:39:58 -0500

    Organizing folders

---
+++ 01_Topics/Inheritance/src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+
+        /*Inheritance = One class inherits the attributes and methods from another class.
+                        Child <- Parent <- Grandparent */
+
+        Dog dog = new Dog();
+        Cat cat = new Cat();
+        Plant plant = new Plant();
+
+        System.out.println(plant.isAlive);
+        plant.photosynthesize();
+    }
+}
